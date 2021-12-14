1c6c718ec2a6272e0dae775f8f3b17410baa998a
nirmiller <nir.miller928@gmail.com>
2021-12-14 13:22:33 -0600

    Hello

--- live_threads.py
+++ live_threads.py
@@ -8,7 +8,7 @@ import pandas as pd
 import trade
 from variables import *
 
-model_name = '/content/drive/MyDrive/StockBot/models/stock_bot_comp/CNN/model_4/model_4_1_30'
+model_name = '/content/drive/MyDrive/StockBot/models/stock_bot_comp/CNN/model_3/model_3_4_20'
 agent = Agent(stocks=stocks, TIME_RANGE=TIME_RANGE, PRICE_RANGE=PRICE_RANGE, is_eval=True, model_name=model_name)
 
 fb = dict()
